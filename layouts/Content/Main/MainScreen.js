449b5e93a0c01d1c02202ac0b0fda2fc27ca4492
jiwon <sschong48@naver.com>
2021-08-13 02:55:45 +0900

    Add modal to MainScreen

--- layouts/Content/Main/MainScreen.js
+++ layouts/Content/Main/MainScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, Image, TouchableOpacity, Modal } from "react-native";
 import MapView from "react-native-maps";
 import { PROVIDER_GOOGLE } from 'react-native-maps';
 
@@ -8,6 +8,10 @@ import { PROVIDER_GOOGLE } from 'react-native-maps';
 class MainScreen extends Component {
     constructor({ props }) {
         super(props);
+
+        this.state = {
+            show: true,
+        }
     }
 
     render() {
@@ -21,12 +25,25 @@ class MainScreen extends Component {
                     longitudeDelta: 0.0421,
                 }}
             >
-                <MapView.Marker 
-                    coordinate={{ 
+                <MapView.Marker
+                    coordinate={{
                         latitude: 35.91393,
-                        longitude: 128.80278, }}
+                        longitude: 128.80278,
+                    }}
 
                 />
+                <Modal
+                    offset={this.state.offset}
+                    visible={this.state.show}
+                    transparent={true}>
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <Text style={styles.modalText}>
+                                    현재 운행중인 정보 보기
+                            </Text>
+                        </View>
+                    </View>
+                </Modal>
             </MapView>
         );
     }
@@ -38,8 +55,32 @@ const styles = StyleSheet.create({
         justifyContent: "flex-end",
         backgroundColor: "#f9e6e9",
     },
-    
-    
+    centeredView: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    modalView: {
+        backgroundColor: 'pink',
+        borderRadius: 40,
+        width: '80%',
+        height: '6%',
+        shadowOpacity: 0.25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '10%'
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        textAlign: 'center',
+        fontSize: 18,
+        color: 'white',
+        fontWeight: 'bold'
+    },
+
 })
 
 export default MainScreen;
